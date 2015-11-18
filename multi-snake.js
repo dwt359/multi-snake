29f47bf5d1fae07101141d2d1ce92680b974c80e
Daniel Troiani <dtroiani9@gmail.com>
2015-11-17 20:11:04 -0600

    remove deprecated function take 2

--- multi-snake.js
+++ multi-snake.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-	res.sendFile('index.html');
+	res.sendFile(__dirname + 'index.html');
 });
 
 http.listen(8080, function(){
