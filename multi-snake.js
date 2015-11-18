5e2d045d731420b928f951bf56d7a77c686d3bed
Daniel Troiani <dtroiani9@gmail.com>
2015-11-17 20:11:59 -0600

    remove deprecated function take 3

--- multi-snake.js
+++ multi-snake.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + 'index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(8080, function(){
